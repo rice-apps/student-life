b56be06cc92065bf5619d52b0ccb20abc4389766
jeffreyxiong <jeffxiong017@gmail.com>
2015-02-09 19:48:13 -0600

    base project files

---
+++ student-life/student-life/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  student-life
+//
+//  Created by Jeffrey Xiong on 2/9/15.
+//  Copyright (c) 2015 Jeffrey Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ student-life/student-life/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  student-life
+//
+//  Created by Jeffrey Xiong on 2/9/15.
+//  Copyright (c) 2015 Jeffrey Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
