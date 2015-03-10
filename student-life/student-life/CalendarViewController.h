b2f4f7c325d99f0381c020a63635687aeed3551e
jeffreyxiong <jeffxiong017@gmail.com>
2015-03-09 20:32:19 -0500

    not much

--- student-life/student-life/CalendarViewController.h
+++ student-life/student-life/CalendarViewController.h
@@ -2,7 +2,7 @@
 //  CalendarViewController.h
 //  student-life
 //
-//  Created by Jeffrey Xiong on 3/8/15.
+//  Created by Jeffrey Xiong on 3/9/15.
 //  Copyright (c) 2015 Jeffrey Xiong. All rights reserved.
 //
 
--- student-life/student-life/CalendarViewController.m
+++ student-life/student-life/CalendarViewController.m
@@ -2,7 +2,7 @@
 //  CalendarViewController.m
 //  student-life
 //
-//  Created by Jeffrey Xiong on 3/8/15.
+//  Created by Jeffrey Xiong on 3/9/15.
 //  Copyright (c) 2015 Jeffrey Xiong. All rights reserved.
 //
 
@@ -25,6 +25,7 @@
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
