a364ac72d902a93e053e114945b03e1f06a41aa5
jakepeacock <james.hoke.peacock@gmail.com>
2015-04-12 21:43:01 -0500

    Started building Facilities Page

---
+++ student-life/student-life/FacilitiesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FacilitiesTableViewController.h
+//  student-life
+//
+//  Created by James Hoke Peacock IV on 4/12/15.
+//  Copyright (c) 2015 Jeffrey Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacilitiesTableViewController : UITableViewController
+
+@end
+
+
