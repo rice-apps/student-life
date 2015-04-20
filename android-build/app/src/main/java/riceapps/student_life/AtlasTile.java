d2e4a281cd6813a337ef1db277c64ae82cd5c5aa
unknown <brianlee.jpg@gmail.com>
2015-04-19 20:13:05 -0500

    activity_atlas 0.01

---
+++ android-build/app/src/main/java/riceapps/student_life/AtlasTile.java
@@ -0,0 +1,16 @@
+package riceapps.student_life;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Brian on 4/19/2015.
+ */
+public class AtlasTile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atlas);
+    }
+
+}
