fba342ad6474efdd3cbc9876c16938dd0f3a9219
jeffreyxiong <jeffxiong017@gmail.com>
2015-09-07 15:10:45 -0500

    swiftify start 2

---
+++ Rice Mobile/Rice Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rice Mobile
+//
+//  Created by Jeffrey Xiong on 9/7/15.
+//  Copyright (c) 2015 Jeffrey Xiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
